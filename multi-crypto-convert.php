26db9784fa5f2947f45db98c438554c3e217d2fc
Carlos Guzman <caralbgm@gmail.com>
2025-12-02 18:43:27 -0500

    Init class to handle the plugin settings.

--- multi-crypto-convert.php
+++ multi-crypto-convert.php
@@ -10,9 +10,21 @@
  * @package Multi_Crypto_Convert
  */
 
+declare( strict_types=1 );
+
+namespace Multi_Crypto_Convert;
+
+use Multi_Crypto_Convert\Cache\Crypto_Option_Cache;
+use Multi_Crypto_Convert\Clients\Crypto_Client_Factory;
+use Multi_Crypto_Convert\Settings\Admin_Settings;
+
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
+
+define( 'MCC_PLUGIN_FILE', __FILE__ );
+define( 'MCC_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+define( 'MCC_PLUGIN_VERSION', '0.1.0' );
 
 /**
  * Load Composer autoloader if present.
@@ -21,3 +33,12 @@ $autoload = __DIR__ . '/vendor/autoload.php';
 if ( file_exists( $autoload ) ) {
 	require_once $autoload;
 }
+
+add_action(
+	'plugins_loaded',
+	function () {
+		$cache = new Crypto_Option_Cache();
+		$factory = new Crypto_Client_Factory( $cache );
+		new Admin_Settings( $factory );
+	}
+);
