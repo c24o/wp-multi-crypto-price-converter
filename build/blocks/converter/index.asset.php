1c68b186d91e51e283f8d1257cf8979e3561ba7a
Carlos Guzman <caralbgm@gmail.com>
2025-12-04 11:40:03 -0500

    Scaffold for the convert block.

---
+++ build/blocks/converter/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'af51e41fc44bed52bb8a');
---
+++ build/blocks/converter/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'd579a33b1d9815034aef');
